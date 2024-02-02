0bfbb57b4c162f4038f0e7318c7846c03ea619ab
Sydney <proteimus@gmail.com>
2024-02-02 13:21:38 -0600

    counter On/OFF state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [isOn] = useState(true)
-
+  const [isOn, setIsOn] = useState(false)
+  const [count, setCount] = useState(0)
+  const toggle = evt => setIsOn(!isOn)     // eslint-disable line
+  const inc = evt => setCount(count + 1)   // eslint-disable line
+ 
   return (
     <div>
-      <h2>Lightbulb</h2>
-      <div>The bulb is {isOn ? 'ON' : 'OFF'}</div>
+      <h2>Lightbulb & Counter</h2>
+      <div onClick={toggle}>The bulb is {isOn ? 'ON' : 'OFF'}</div>
+      <div onClick={inc}>The count is {count}</div>
     </div>
   )
 }
 
-export default App
+export default App 
